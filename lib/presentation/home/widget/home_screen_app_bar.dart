8a212c7ad3c1f9683b41ded95adf00db3fe528a5
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-01 20:03:38 +0900

    add :: home_screen_app_bar 추가

---
+++ lib/presentation/home/widget/home_screen_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:moatikon_flutter/core/component/image_widget.dart';
+
+class HomeScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomeScreenAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Row(
+        children: [
+          ImageWidget(
+            image: 'assets/image/logo_with_text.png',
+            width: 96.w,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(52.h);
+}
