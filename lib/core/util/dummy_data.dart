d0b4add268fe76ef34118c5b6e0fa654f288df73
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-05 16:59:01 +0900

    update :: dummy_data 수정

--- lib/core/util/dummy_data.dart
+++ lib/core/util/dummy_data.dart
@@ -11,11 +11,11 @@ List<TikonDto> dummyData = [
   ),
 
   const TikonDto(
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/500px-Pizza-3007395.jpg",
-    storeName: "Pizza Hut",
-    tikonName: "맛있는 피자 할인권",
-    category: "식사류",
-    dDay: 1,
+    image: "https://www.contis.ph/cdn/shop/products/CokeinCan.jpg?v=1689558538&width=1200",
+    storeName: "코카콜라",
+    tikonName: "코카콜라가 무료!",
+    category: "음료",
+    dDay: 60,
     disCount: 100,
   ),
 
