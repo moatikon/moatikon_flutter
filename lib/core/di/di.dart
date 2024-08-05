1c87aea7dd490e2f8d2f907dd0c91e7e388fc355
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-05 11:32:00 +0900

    update :: tikon di 적용

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -1,8 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moatikon_flutter/data/tikon/data_source/remote/remote_tikon_data_source.dart';
+import 'package:moatikon_flutter/data/tikon/repository/tikon_repository_impl.dart';
+import 'package:moatikon_flutter/domain/tikon/use_case/get_all_tikon_list_use_case.dart';
 import 'package:moatikon_flutter/presentation/home/view_model/tag_state.dart';
+import 'package:moatikon_flutter/presentation/home/view_model/tikon_bloc.dart';
 
 Future<List<BlocProvider>> di() async {
+
+  // tikon
+  RemoteTikonDataSource remoteTikonDataSource = RemoteTikonDataSource();
+  TikonRepositoryImpl tikonRepositoryImpl = TikonRepositoryImpl(remoteTikonDataSource: remoteTikonDataSource);
+  GetAllTikonListUseCase getAllTikonListUseCase = GetAllTikonListUseCase(tikonRepository: tikonRepositoryImpl);
+
   return [
     BlocProvider<TagState>(create: (context) => TagState()),
+    BlocProvider<TikonBloc>(create: (context) => TikonBloc(getAllTikonListUseCase: getAllTikonListUseCase))
   ];
 }
