0ad3a0786d7a38251162195a5f60abffce595eaa
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-05 11:39:15 +0900

    update :: tag_list_widget BlocBuilder 적용

--- lib/presentation/home/ui/widget/tag_list_widget.dart
+++ lib/presentation/home/ui/widget/tag_list_widget.dart
@@ -9,42 +9,45 @@ class TagListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TagState tagState = context.read<TagState>();
     List<String> tagList = ["전체", "식사류", "음료", "물건", "기타"];
 
-    return SizedBox(
-      height: 25.h,
-      child: ListView.builder(
-        itemCount: tagList.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          bool isSelected = context.watch<TagState>().state == index;
+    return BlocBuilder<TagState, int>(
+      builder: (context, state) {
+        return SizedBox(
+          height: 25.h,
+          child: ListView.builder(
+            itemCount: tagList.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              bool isSelected = state == index;
 
-          return Padding(
-            padding: EdgeInsets.only(right: 10.w),
-            child: GestureDetector(
-              onTap: () => tagState.changeState(index),
-              child: Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: isSelected ? const Color(0xFFEA434D) : const Color(0xFFFFFFFF),
-                  border: Border.all(color: isSelected ? const Color(0xFFEA434D) : const Color(0xFF939493)),
-                  borderRadius: BorderRadius.circular(30.r)
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.w),
-                  child: TextWidget(
-                    text: tagList[index],
-                    textSize: 13.sp,
-                    textWeight: TextWeight.semiBold,
-                    color: isSelected ? const Color(0xFFFFFFFF) : const Color(0xFF939493),
+              return Padding(
+                padding: EdgeInsets.only(right: 10.w),
+                child: GestureDetector(
+                  onTap: () => context.read<TagState>().changeState(index),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: isSelected ? const Color(0xFFEA434D) : const Color(0xFFFFFFFF),
+                        border: Border.all(color: isSelected ? const Color(0xFFEA434D) : const Color(0xFF939493)),
+                        borderRadius: BorderRadius.circular(30.r)
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.w),
+                      child: TextWidget(
+                        text: tagList[index],
+                        textSize: 13.sp,
+                        textWeight: TextWeight.semiBold,
+                        color: isSelected ? const Color(0xFFFFFFFF) : const Color(0xFF939493),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          );
-        },
-      ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
