fe4b882693ecd0ed573d38df12bbff20ba36f51f
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-05 12:03:56 +0900

    add :: dummy_data 추가 및 적용

---
+++ lib/core/util/dummy_data.dart
@@ -0,0 +1,30 @@
+import '../../data/tikon/dto/response/tikon_dto.dart';
+
+List<TikonDto> dummyData = [
+  const TikonDto(
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/500px-Pizza-3007395.jpg",
+    storeName: "Pizza Hut",
+    tikonName: "맛있는 피자 할인권",
+    category: "식사류",
+    dDay: 1,
+    disCount: 100,
+  ),
+
+  const TikonDto(
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/500px-Pizza-3007395.jpg",
+    storeName: "Pizza Hut",
+    tikonName: "맛있는 피자 할인권",
+    category: "식사류",
+    dDay: 1,
+    disCount: 100,
+  ),
+
+  const TikonDto(
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/500px-Pizza-3007395.jpg",
+    storeName: "Pizza Hut",
+    tikonName: "맛있는 피자 할인권",
+    category: "식사류",
+    dDay: 1,
+    disCount: 100,
+  ),
+];
