f4f9deb71b88eaa0a88a3d10e0b47d72f5120e34
ljyo2o9 <yggosxoljy@gmail.com>
2024-08-05 16:55:41 +0900

    update :: tikon_model 삭제

--- lib/presentation/home/view_model/tikon_bloc.dart
+++ lib/presentation/home/view_model/tikon_bloc.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moatikon_flutter/domain/tikon/entity/tikon_entity.dart';
 import 'package:moatikon_flutter/domain/tikon/use_case/get_all_tikon_list_use_case.dart';
 import 'package:moatikon_flutter/presentation/home/view_model/tikon_event.dart';
-import 'package:moatikon_flutter/presentation/home/view_model/tikon_model.dart';
 import 'package:moatikon_flutter/presentation/home/view_model/tikon_state.dart';
 
-class TikonBloc extends Bloc<TikonEvent, TikonState<TikonModel>> {
+class TikonBloc extends Bloc<TikonEvent, TikonState<List<TikonEntity>>> {
   final GetAllTikonListUseCase _getAllTikonList;
 
   TikonBloc({required GetAllTikonListUseCase getAllTikonListUseCase})
@@ -14,12 +14,11 @@ class TikonBloc extends Bloc<TikonEvent, TikonState<TikonModel>> {
   }
 
   void _getAllTikonListHandler(GetAllTikonListEvent event,
-      Emitter<TikonState<TikonModel>> emit) async {
+      Emitter<TikonState<List<TikonEntity>>> emit) async {
     try{
       emit(Loading());
       final newEntity = await _getAllTikonList.execute();
-      final newModel = TikonModel(tikonList: newEntity);
-      emit(Loaded(data: newModel));
+      emit(Loaded(data: newEntity));
     } catch(err){
       emit(Error(exception: err));
     }
