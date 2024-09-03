adb657f8a70707401b4fdac7bd5e67aca958499a
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-03 13:26:56 +0900

    add :: dio intercepcotr

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:moatikon_flutter/core/dio_init.dart';
 import 'package:moatikon_flutter/presentation/splash/ui/view/splash_screen.dart';
 
 import 'di/di.dart';
 
 void main() async {
+  dioInit();
+
   runApp(MyApp(
     blocProvider: await di(),
   ));
