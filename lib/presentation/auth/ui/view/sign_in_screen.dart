605cf7f4c34a6b2bc5216a0a7ea4c7ef6ad58b08
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-12 11:07:12 +0900

    update :: add ToastMessgae

--- lib/presentation/auth/ui/view/sign_in_screen.dart
+++ lib/presentation/auth/ui/view/sign_in_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:moatikon_flutter/component/moa_button.dart';
 import 'package:moatikon_flutter/component/my_scaffold.dart';
 import 'package:moatikon_flutter/component/text_widget.dart';
+import 'package:moatikon_flutter/component/toast_message.dart';
 import 'package:moatikon_flutter/core/moa_navigator.dart';
 import 'package:moatikon_flutter/data/auth/dto/request/auth_request.dart';
 import 'package:moatikon_flutter/presentation/auth/ui/view/sign_up_screen.dart';
@@ -12,9 +13,10 @@ import 'package:moatikon_flutter/presentation/auth/ui/widget/auth_rich_text_widg
 import 'package:moatikon_flutter/presentation/auth/ui/widget/auth_text_field_widget.dart';
 import 'package:moatikon_flutter/presentation/auth/view_model/auth_bloc.dart';
 import 'package:moatikon_flutter/presentation/auth/view_model/auth_event.dart';
-import 'package:moatikon_flutter/presentation/auth/view_model/auth_state.dart';
 import 'package:moatikon_flutter/presentation/tikon/ui/view/home_screen.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
+import '../../../../core/bloc/bloc_state_none_value.dart';
 import '../../../../core/bloc_state_enum.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -53,46 +55,39 @@ class _SignInScreenState extends State<SignInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    TextWidget? checkErrorText(AuthState state) {
-      if (state.authState == BlocStateEnum.error) {
-        return TextWidget(
-          text: "유저 정보와 일치하지 않습니다.",
-          textSize: 12.sp,
-          textWeight: TextWeight.regular,
-          color: const Color(0xFFF5290A),
-        );
-      }
-
-      return null;
-    }
-
     return MyScaffold(
       appbar: const AuthAppBar(title: "로그인"),
       bottomSheet: Padding(
         padding: EdgeInsets.only(bottom: 20.h),
         child: GestureDetector(
-            onTap: () => MoaNavigator.pushReplacement(context, const SignUpScreen()),
-            child: const AuthRichTextWidget(
-              firstText: "모아티콘이 처음이라면?",
-              secondText: "회원가입",
-            )),
+          onTap: () => MoaNavigator.pushReplacement(context, const SignUpScreen()),
+          child: const AuthRichTextWidget(
+            firstText: "모아티콘이 처음이라면?",
+            secondText: "회원가입",
+          ),
+        ),
       ),
       body: MultiBlocListener(
         listeners: [
-          BlocListener<AuthBloc, AuthState>(
-            listenWhen: (_, current) => current.authState == BlocStateEnum.loaded,
+          BlocListener<AuthBloc, BlocState>(
+            listenWhen: (_, current) => current.blocState == BlocStateEnum.loaded,
             listener: (_, __) => MoaNavigator.go(context, const HomeScreen()),
           ),
 
-          BlocListener<AuthBloc, AuthState>(
-            listenWhen: (_, current) => current.authState == BlocStateEnum.error,
-            listener: (_, __) {
-              _emailController.clear();
-              _passwordController.clear();
+          BlocListener<AuthBloc, BlocState>(
+            listenWhen: (_, current) => current.blocState == BlocStateEnum.error,
+            listener: (_, state) {
+              showTopSnackBar(
+                Overlay.of(context),
+                ToastMessage(
+                  isError: true,
+                  title: state.error.message[0],
+                ),
+              );
             },
           ),
         ],
-        child: BlocBuilder<AuthBloc, AuthState>(builder: (_, state) {
+        child: BlocBuilder<AuthBloc, BlocState>(builder: (_, state) {
           return Padding(
             padding: EdgeInsets.all(20.w),
             child: Column(
@@ -101,7 +96,6 @@ class _SignInScreenState extends State<SignInScreen> {
                   controller: _emailController,
                   node: _emailNode,
                   hintText: "이메일",
-                  errorWidget: checkErrorText(state),
                 ),
                 SizedBox(height: 20.h),
                 AuthTextFieldWidget(
@@ -109,20 +103,20 @@ class _SignInScreenState extends State<SignInScreen> {
                   node: _passwordNode,
                   hintText: "비밀번호",
                   isObscure: true,
-                  errorWidget: checkErrorText(state),
                 ),
                 SizedBox(height: 30.h),
                 MoaButton(
                   onTap: () {
-                    if(_emailController.text.isNotEmpty && _passwordController.text.isNotEmpty){
+                    if (_emailController.text.isNotEmpty &&
+                        _passwordController.text.isNotEmpty) {
                       context.read<AuthBloc>().add(
-                        SignInEvent(
-                          authRequest: AuthRequest(
-                            email: _emailController.text,
-                            password: _passwordController.text,
-                          ),
-                        ),
-                      );
+                            SignInEvent(
+                              authRequest: AuthRequest(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                              ),
+                            ),
+                          );
                     }
                   },
                   text: "로그인",
