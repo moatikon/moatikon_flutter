ea213dbb141f41238e55b4c2a394c17a4d4a9345
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-12 11:21:10 +0900

    update :: sign_up error text

--- lib/presentation/auth/ui/view/sign_up_screen.dart
+++ lib/presentation/auth/ui/view/sign_up_screen.dart
@@ -13,8 +13,9 @@ import 'package:moatikon_flutter/presentation/auth/ui/widget/auth_app_bar.dart';
 import 'package:moatikon_flutter/presentation/auth/ui/widget/auth_text_field_widget.dart';
 import 'package:moatikon_flutter/presentation/auth/view_model/auth_bloc.dart';
 import 'package:moatikon_flutter/presentation/auth/view_model/auth_event.dart';
-import 'package:moatikon_flutter/presentation/auth/view_model/text_field_error_func.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
+import '../../../../component/toast_message.dart';
 import '../../../tikon/ui/view/home_screen.dart';
 import '../widget/auth_rich_text_widget.dart';
 
@@ -69,28 +70,32 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    bool textFieldAllCorrect(BlocState authState) {
-      if(_emailController.text.isEmpty && _passwordController.text.isEmpty && _passwordCompareController.text.isEmpty){
-        return false;
+    RegExp regex = RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+    TextWidget? emailErrorText(){
+      if(_emailController.text.isNotEmpty &&
+          !regex.hasMatch(_emailController.text)){
+        return TextWidget(
+          text: "이메일 형식으로 입력해주세요",
+          textSize: 12.sp,
+          textWeight: TextWeight.regular,
+          color: const Color(0xFFF5290A),
+        );
       }
 
-      TextWidget? duplicationUserText = TextFieldErrorFunc.emailText(
-        state: authState,
-        emailController: _emailController,
-      );
-      TextWidget? passwordNotMatchText =
-          TextFieldErrorFunc.passwordNotMatchText(
-        passwordController: _passwordController,
-      );
-      TextWidget? passWordNotCompareText =
-          TextFieldErrorFunc.passWordNotCompareText(
-        passwordController: _passwordController,
-        passwordCompareController: _passwordCompareController,
-      );
+      return null;
+    }
 
-      return duplicationUserText == null &&
-          passwordNotMatchText == null &&
-          passWordNotCompareText == null;
+    TextWidget? passwordErrorText(){
+      if(_passwordController.text != _passwordCompareController.text){
+        return TextWidget(
+          text: "비밀번호가 일치하지 않습니다.",
+          textSize: 12.sp,
+          textWeight: TextWeight.regular,
+          color: const Color(0xFFF5290A),
+        );
+      }
+
+      return null;
     }
 
     return MyScaffold(
@@ -115,10 +120,14 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
           BlocListener<AuthBloc, BlocState>(
             listenWhen: (_, current) => current.blocState == BlocStateEnum.error,
-            listener: (_, __) {
-              _emailController.clear();
-              _passwordController.clear();
-              _passwordCompareController.clear();
+            listener: (_, state) {
+              showTopSnackBar(
+                Overlay.of(context),
+                ToastMessage(
+                  isError: true,
+                  title: state.error.message[0],
+                ),
+              );
             },
           ),
         ],
@@ -132,10 +141,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     controller: _emailController,
                     node: _emailNode,
                     hintText: "이메일",
-                    errorWidget: TextFieldErrorFunc.emailText(
-                      state: state,
-                      emailController: _emailController,
-                    ),
+                    errorWidget: emailErrorText(),
                   ),
                   SizedBox(height: 20.h),
                   AuthTextFieldWidget(
@@ -143,9 +149,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     node: _passwordNode,
                     hintText: "비밀번호",
                     isObscure: true,
-                    errorWidget: TextFieldErrorFunc.passwordNotMatchText(
-                      passwordController: _passwordController,
-                    ),
+                    errorWidget: passwordErrorText(),
                   ),
                   SizedBox(height: 20.h),
                   AuthTextFieldWidget(
@@ -153,21 +157,21 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     node: _passwordCompareNode,
                     hintText: "비밀번호 재입력",
                     isObscure: true,
-                    errorWidget: TextFieldErrorFunc.passWordNotCompareText(
-                      passwordController: _passwordController,
-                      passwordCompareController: _passwordCompareController,
-                    ),
+                    errorWidget: passwordErrorText(),
                   ),
                   SizedBox(height: 30.h),
                   MoaButton(
                     onTap: () {
-                      context.read<AuthBloc>().add(EmptyEvent());
-                      if (textFieldAllCorrect(state)) {
-                        context.read<AuthBloc>().add(SignUpEvent(
+                      if (emailErrorText() == null &&
+                          passwordErrorText() == null) {
+                        context.read<AuthBloc>().add(
+                              SignUpEvent(
                                 authRequest: AuthRequest(
-                              email: _emailController.text,
-                              password: _passwordController.text,
-                            )));
+                                  email: _emailController.text,
+                                  password: _passwordController.text,
+                                ),
+                              ),
+                            );
                       }
                     },
                     text: "회원가입",
