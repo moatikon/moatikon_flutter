8cda1d964256d7d15967fa1c4316c6419c5e76b3
ljyo2o9 <yggosxoljy@gmail.com>
2025-02-16 16:38:22 +0900

    add :: moa_color

---
+++ lib/core/moa_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MoaColor {
+  static const Color red200 = Color(0xFFF5290A);
+  static const Color red100 = Color(0xFFEA434D);
+  static const Color red50 = Color(0xFFF5E8E6);
+  static const Color gray100 = Color(0xFFD9D9D9);
+  static const Color gray200 = Color(0xFF939599);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
