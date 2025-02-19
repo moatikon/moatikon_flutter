a59b4c32ab93c3692a9d95f20bf079a0829352b5
ljyo2o9 <yggosxoljy@gmail.com>
2025-02-19 23:52:24 +0900

    update :: add_tikon_screen listener 추가

--- lib/presentation/add_tikon/ui/add_tikon_screen.dart
+++ lib/presentation/add_tikon/ui/add_tikon_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:moatikon_flutter/component/my_scaffold.dart';
+import 'package:moatikon_flutter/component/toast_message.dart';
+import 'package:moatikon_flutter/core/bloc/bloc_state_enum.dart';
+import 'package:moatikon_flutter/core/bloc/bloc_state_none_value.dart';
+import 'package:moatikon_flutter/core/moa_navigator.dart';
 import 'package:moatikon_flutter/presentation/add_tikon/ui/widget/add_tikon_app_bar.dart';
 import 'package:moatikon_flutter/presentation/add_tikon/ui/widget/add_tikon_bottom_sheet.dart';
 import 'package:moatikon_flutter/presentation/add_tikon/ui/widget/add_tikon_calender_widget.dart';
@@ -8,6 +13,8 @@ import 'package:moatikon_flutter/presentation/add_tikon/ui/widget/add_tikon_cate
 import 'package:moatikon_flutter/presentation/add_tikon/ui/widget/add_tikon_image_button_widget.dart';
 import 'package:moatikon_flutter/presentation/add_tikon/ui/widget/add_tikon_slider_widget.dart';
 import 'package:moatikon_flutter/presentation/add_tikon/ui/widget/add_tikon_text_field_widget.dart';
+import 'package:moatikon_flutter/presentation/add_tikon/view_model/add_tikon_bloc.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 class AddTikonScreen extends StatefulWidget {
   const AddTikonScreen({super.key});
@@ -45,39 +52,62 @@ class _AddTikonScreenState extends State<AddTikonScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MyScaffold(
-      appbar: const AddTikonAppBar(),
-      bottomSheet: AddTikonBottomSheet(
-        tikonNameController: _tikonNameController,
-        storeNameController: _storeNameController,
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 30.h, left: 20.w, right: 20.w),
-        child: Center(
-          child: Column(
-            children: [
-              const AddTikonImageButtonWidget(),
-              SizedBox(height: 20.h),
-              AddTikonTextFieldWidget(
-                title: "기프티콘명",
-                hintText: "기프티콘에 이름을 지어주세요!",
-                controller: _tikonNameController,
-                node: _tikonNameNode,
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<AddTikonBloc, BlocStateNoneValue>(
+          listenWhen: (_, current) => current.blocState == BlocStateEnum.loaded,
+          listener: (context, state) {
+            MoaNavigator.pop(context);
+          },
+        ),
+
+        BlocListener<AddTikonBloc, BlocStateNoneValue>(
+          listenWhen: (_, current) => current.blocState == BlocStateEnum.error,
+          listener: (context, state) {
+            showTopSnackBar(
+              Overlay.of(context),
+              ToastMessage(
+                title: state.error.message,
+                isError: true,
               ),
-              SizedBox(height: 20.h),
-              AddTikonTextFieldWidget(
-                title: "기프티콘 사용처",
-                hintText: "어디서 사용하나요? ex) 모아티콘",
-                controller: _storeNameController,
-                node: _storeNameNode,
-              ),
-              SizedBox(height: 20.h),
-              const AddTikonCalenderWidget(),
-              SizedBox(height: 20.h),
-              const AddTikonSliderWidget(),
-              SizedBox(height: 20.h),
-              const AddTikonCategoryWidget()
-            ],
+            );
+          },
+        ),
+      ],
+      child: MyScaffold(
+        appbar: const AddTikonAppBar(),
+        bottomSheet: AddTikonBottomSheet(
+          tikonNameController: _tikonNameController,
+          storeNameController: _storeNameController,
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(top: 30.h, left: 20.w, right: 20.w),
+          child: Center(
+            child: Column(
+              children: [
+                const AddTikonImageButtonWidget(),
+                SizedBox(height: 20.h),
+                AddTikonTextFieldWidget(
+                  title: "기프티콘명",
+                  hintText: "기프티콘에 이름을 지어주세요!",
+                  controller: _tikonNameController,
+                  node: _tikonNameNode,
+                ),
+                SizedBox(height: 20.h),
+                AddTikonTextFieldWidget(
+                  title: "기프티콘 사용처",
+                  hintText: "어디서 사용하나요? ex) 모아티콘",
+                  controller: _storeNameController,
+                  node: _storeNameNode,
+                ),
+                SizedBox(height: 20.h),
+                const AddTikonCalenderWidget(),
+                SizedBox(height: 20.h),
+                const AddTikonSliderWidget(),
+                SizedBox(height: 20.h),
+                const AddTikonCategoryWidget()
+              ],
+            ),
           ),
         ),
       ),
