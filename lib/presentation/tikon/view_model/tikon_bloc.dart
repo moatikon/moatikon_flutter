fc4ba0d01f765d9bf3fa0562f4eb69b6f35bed29
ljyo2o9 <yggosxoljy@gmail.com>
2025-02-19 23:22:34 +0900

    delete :: add_tikon old code

--- lib/presentation/tikon/view_model/tikon_bloc.dart
+++ lib/presentation/tikon/view_model/tikon_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moatikon_flutter/core/bloc/bloc_state_value.dart';
-import 'package:moatikon_flutter/domain/tikon/use_case/add_tikon_use_case.dart';
 import 'package:moatikon_flutter/domain/tikon/use_case/get_all_tikon_list_use_case.dart';
 import 'package:moatikon_flutter/presentation/tikon/view_model/tikon_event.dart';
 import '../../../domain/tikon/entity/tikons_entity.dart';
@@ -9,29 +8,15 @@ import '../../../domain/tikon/use_case/complete_tikon_use_case.dart';
 
 class TikonBloc extends Bloc<TikonEvent, BlocState<TikonsEntity>> {
   final GetAllTikonListUseCase _getAllTikonListUseCase;
-  final AddTikonUseCase _addTikonUseCase;
   final CompleteTikonUseCase _completeTikonUseCase;
 
   TikonBloc({
     required GetAllTikonListUseCase getAllTikonListUseCase,
-    required AddTikonUseCase addTikonUseCase,
     required CompleteTikonUseCase completeTikonUseCase,
   })  : _getAllTikonListUseCase = getAllTikonListUseCase,
-        _addTikonUseCase = addTikonUseCase,
         _completeTikonUseCase = completeTikonUseCase,
         super(Empty()) {
-    on<AddTikonEvent>(_addTikonHandler);
     on<CompleteTikonEvent>(_completeTikonHandler);
-  }
-
-  Future<void> _addTikonHandler(
-    AddTikonEvent event,
-    Emitter<BlocState<TikonsEntity>> emit,
-  ) async {
-    emit(Loading());
-    await _addTikonUseCase.execute(event.addTikonRequest);
-    final TikonsEntity tikonEntity = await _getAllTikonListUseCase.execute();
-    emit(Loaded(data: tikonEntity));
   }
 
   Future<void> _completeTikonHandler(
