912e93e2b25514a7305837c10316d0a2334125ed
ljyo2o9 <yggosxoljy@gmail.com>
2025-02-27 18:02:01 +0900

    update :: add moa_color ( Green100 )

--- lib/core/moa_color.dart
+++ lib/core/moa_color.dart
@@ -6,6 +6,7 @@ class MoaColor {
   static const Color red50 = Color(0xFFF5E8E6);
   static const Color gray100 = Color(0xFFD9D9D9);
   static const Color gray200 = Color(0xFF939599);
+  static const Color green100 = Color(0xFF599850);
   static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFFFFFFF);
 }
